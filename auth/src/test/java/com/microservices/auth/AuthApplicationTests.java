bf618806f87a6ace3ffbe29fb38e01e5842501ed
leonardocerqueira69 <leonardosilvac89@gmail.com>
2025-04-16 22:44:16 -0300

    teste de integração com testcontainers

--- auth/src/test/java/com/microservices/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/microservices/auth/AuthApplicationTests.java
@@ -1,15 +1,83 @@
 package com.microservices.auth;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class AuthApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:17.4")
+            .withDatabaseName("authdb")
+            .withUsername("postgres")
+            .withPassword("postgres");
 
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @Test
+    void shouldRegisterAndAuthenticateUser() {
+        String registerBody = """
+                {
+                    "name": "Leo Test",
+                    "email": "leo@test.com",
+                    "password": "123456",
+                    "role": "USER"
+                }
+                """;
+
+        // Testa o registro
+        given()
+            .contentType("application/json")
+            .body(registerBody)
+        .when()
+            .post("/auth/register")
+        .then()
+            .statusCode(200)
+            .body(containsString("Usuário cadastrado com sucesso"));
+
+        String loginBody = """
+                {
+                    "email": "leo@test.com",
+                    "password": "123456"
+                }
+                """;
+
+        // Testa o login
+        given()
+            .contentType("application/json")
+            .body(loginBody)
+        .when()
+            .post("/auth/login")
+        .then()
+            .statusCode(200)
+            .body("token", notNullValue())
+            .body("name", equalTo("Leo Test"));
+    }
 }
